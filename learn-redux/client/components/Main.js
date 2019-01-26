fc4bd48def4950a4b78945b55d6c028c909d2bc6
Parmeet <parmeet.asi@gmail.com>
2019-01-26 18:44:22 +0530

    added header component

---
+++ learn-redux/client/components/Main.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { link } from "react-router";
+const Main = React.createClass({
+  render() {
+    return (
+      <div>
+        <h1>Reduxstagram</h1>
+      </div>
+    );
+  }
+});
+
+export default Main;
